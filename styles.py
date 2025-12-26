f56868522748cc5abf3a75a3eb821ab38f059728
Kirutika K S <kirutikabe@gmail.com>
2025-12-26 19:47:37 +0530

    Python file

---
+++ styles.py
@@ -0,0 +1,21 @@
+#-----styles.py-----
+LIGHT = {
+    "bg": "white",
+    "fg": "black",
+    "entry_bg": "white",
+    "entry_fg": "black",
+    "accent": "#444444"
+}
+
+DARK = {
+    "bg": "#1e1e1e",
+    "fg": "white",
+    "entry_bg": "#333333",
+    "entry_fg": "white",
+    "accent": "#bbbbbb"
+}
+
+
+
+
+
